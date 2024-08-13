5f8b4fd8677fcfb6606bda99d6fc1691874edd67
Neto <j.altevirneto@gmail.com>
2024-08-13 10:35:57 -0300

    saving

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
-app.get("/geada", (req, res) => {
+app.use(express.json());
+
+app.get("/:usuario/:repositorio", (req, res) => {
+    console.log("Antes do ?:", req.params.usuario, req.params.repositorio)
+    console.log("Depois do ?: ", req.query.id, req.query.idade)
+    console.log("Corpo da requisição:", req.body)
     res.send("Isfrio");
 })
 
